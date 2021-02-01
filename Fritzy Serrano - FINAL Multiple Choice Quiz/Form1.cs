e1e654278eb7839cd3b1f00f34776083caa6b8f1
Fritzy-gif <66807756+Fritzy-gif@users.noreply.github.com>
2021-02-02 10:42:01 +1100

    Add files via upload
    this is a back up c-social-ethical

---
+++ Fritzy Serrano - FINAL Multiple Choice Quiz/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fritzy_Serrano___FINAL_Multiple_Choice_Quiz
+{
+    public partial class openingScreen : Form
+    {
+        public openingScreen()
+        {
+            InitializeComponent();
+        }
+        
+        private void startButton1_Click_1(object sender, EventArgs e)
+        {
+            //when the 'Intructions' button is clicked, openingScreen will go invisible and move to Instructions
+            this.Visible = false; //note that the 'Visible' property is a boolean
+            Instructions form2 = new Instructions();
+            form2.Show();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
